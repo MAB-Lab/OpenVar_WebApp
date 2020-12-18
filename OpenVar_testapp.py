6e7cde02a8f7d2e2399fb95d7845cd3d0d7b6046
MAB-Lab <marie.brunet@usherbrooke.ca>
2020-12-18 16:33:21 -0500

    Update upload

--- OpenVar_testapp.py
+++ OpenVar_testapp.py
@@ -7,7 +7,7 @@ app.config['MAX_CONTENT_LENGTH'] = 25 * 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.txt', '.tsv', '.csv']
 app.config['UPLOAD_PATH'] = 'uploads'
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=["GET"])
 def home():
     return render_template("Home.html")
 
@@ -19,6 +19,21 @@ def help():
 def submit():
     print(request.form)
     return render_template("OPVar_submit.html")
+
+@app.route("/submit", methods=["POST"])
+def upload_file():
+    print('Starting upload')
+    uploaded = request.files['file']
+    upload_name = secure_filename(uploaded.filename)
+    if upload_name != '':
+        ext = os.path.splitext(upload_name)[1]
+        if ext not in app.config['UPLOAD_EXTENSIONS']:
+            return 'Invalid file format.'
+        else:
+            uploaded.save(os.path.join(app.config['UPLOAD_PATH'], upload_name))
+            return '', 204
+    else:
+        return 'ERROR: not file selected'
 
 @app.errorhandler(413)
 def too_large(e):
