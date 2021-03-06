2dfb5b97e1bc528b8fa38c1d51af846718cb4804
MAB-Lab <marie.brunet@usherbrooke.ca>
2021-03-06 16:23:03 -0500

    display ref only

--- static/js/OPV_results.js
+++ static/js/OPV_results.js
@@ -107,122 +107,128 @@ $(document).ready(function() {
 					$('.results__gene_allnulls').css('display', 'grid');
 				}
 
-				if (data.prot_allnulls == 'False') {
-					$('.results__protein').css('display', 'grid');
-					$(function() {
-						$.each(data.prot_stats, function(key, item){
-							$('<tr>').append(
-							$('<td>').text(key),
-							$('<td>').text(item.toLocaleString()).css('font-family', 'Courier').css('text-align', 'right')).appendTo('#results_prot_tbody');
+				console.log(Object.keys(data.general_stats).length);
+				if (Object.keys(data.general_stats).length == 6) {
+					if (data.prot_allnulls == 'False') {
+						$('.results__protein').css('display', 'grid');
+						$(function() {
+							$.each(data.prot_stats, function(key, item){
+								$('<tr>').append(
+								$('<td>').text(key),
+								$('<td>').text(item.toLocaleString()).css('font-family', 'Courier').css('text-align', 'right')).appendTo('#results_prot_tbody');
+							});
 						});
-					});
-					$(function() {
-						$.each(data.prot_counts, function(key, item){
-							$('<tr>').append(
-							$('<td>').text(key),
-							$('<td>').text((Math.round(item.Low*100)/100).toLocaleString()).css('font-family', 'Courier'),
-							$('<td>').text((Math.round(item.Medium*100)/100).toLocaleString()).css('font-family', 'Courier'),
-							$('<td>').text((Math.round(item.High*100)/100).toLocaleString()).css('font-family', 'Courier')).appendTo('#results_protFC_tbody');
+						$(function() {
+							$.each(data.prot_counts, function(key, item){
+								$('<tr>').append(
+								$('<td>').text(key),
+								$('<td>').text((Math.round(item.Low*100)/100).toLocaleString()).css('font-family', 'Courier'),
+								$('<td>').text((Math.round(item.Medium*100)/100).toLocaleString()).css('font-family', 'Courier'),
+								$('<td>').text((Math.round(item.High*100)/100).toLocaleString()).css('font-family', 'Courier')).appendTo('#results_protFC_tbody');
+							});
 						});
-					});
 
-					var bar_FC = document.getElementById('prot__FCbarChart');
-					var FC_bar = new Chart(bar_FC, {
-						type:'horizontalBar',
-						data:{
-							labels: Object.keys(data.prot_counts['Fold Change']),
-							datasets:[{
-								label: 'Fold-change (gain from deeper annotation)',
-								data: Object.values(data.prot_counts['Fold Change']),
-								backgroundColor: ['#e6f2ff', '#66b3ff', '#004d99']
-							}]
-						},
-						options:{ legend:{ display: false }, layout:{ padding: { left: 0, right: 0, top: 0, bottom: 0 } }, scales:{ xAxes:[{scaleLabel:{display:true, labelString: 'Fold-change'}}] }, title: { display: true, text: 'Fold-change gained from deeper annotation per impact', fontSize: 16} },
-					});
+						var bar_FC = document.getElementById('prot__FCbarChart');
+						var FC_bar = new Chart(bar_FC, {
+							type:'horizontalBar',
+							data:{
+								labels: Object.keys(data.prot_counts['Fold Change']),
+								datasets:[{
+									label: 'Fold-change (gain from deeper annotation)',
+									data: Object.values(data.prot_counts['Fold Change']),
+									backgroundColor: ['#e6f2ff', '#66b3ff', '#004d99']
+								}]
+							},
+							options:{ legend:{ display: false }, layout:{ padding: { left: 0, right: 0, top: 0, bottom: 0 } }, scales:{ xAxes:[{scaleLabel:{display:true, labelString: 'Fold-change'}}] }, title: { display: true, text: 'Fold-change gained from deeper annotation per impact', fontSize: 16} },
+						});
 
-					var stackedbar_imp = document.getElementById('prot__ImpactbarChart');
-					var imp_stackedbar = new Chart(stackedbar_imp, {
-						type: 'horizontalBar',
-						data: {
-							labels: Object.keys(data.graph_counts['Alternative protein']),
-							datasets:[{
-								label: 'In known proteins',
-								data: Object.values(data.graph_counts['Reference protein']),
-								backgroundColor: '#39ac73'
-							}, {
-								label: 'In alternative proteins',
-								data: Object.values(data.graph_counts['Alternative protein']),
-								backgroundColor: '#9fdfbf'
-							}]
-						},
-						options: {legend: {display: true, position: 'bottom'}, layout:{ padding: {left: 0, right:0, top: 0, bottom: 0} }, scales:{ xAxes:[{scaleLabel:{display:true, labelString: 'Count of genomic variants'}, stacked: true}], yAxes: [{stacked: true}] }, title: { display: true, text: 'Number of variants per impact', fontSize: 16} },
-					});
+						var stackedbar_imp = document.getElementById('prot__ImpactbarChart');
+						var imp_stackedbar = new Chart(stackedbar_imp, {
+							type: 'horizontalBar',
+							data: {
+								labels: Object.keys(data.graph_counts['Alternative protein']),
+								datasets:[{
+									label: 'In known proteins',
+									data: Object.values(data.graph_counts['Reference protein']),
+									backgroundColor: '#39ac73'
+								}, {
+									label: 'In alternative proteins',
+									data: Object.values(data.graph_counts['Alternative protein']),
+									backgroundColor: '#9fdfbf'
+								}]
+							},
+							options: {legend: {display: true, position: 'bottom'}, layout:{ padding: {left: 0, right:0, top: 0, bottom: 0} }, scales:{ xAxes:[{scaleLabel:{display:true, labelString: 'Count of genomic variants'}, stacked: true}], yAxes: [{stacked: true}] }, title: { display: true, text: 'Number of variants per impact', fontSize: 16} },
+						});
+					} else {
+						$('.results__protein_allnulls').css('display', 'grid');
+					}
+
+					if (data.hotspots_allnulls == 'False') {
+						$('.results__altHotSpot').css('display', 'grid');
+						$('#see_all__hotspots').attr('href', ("/openvar/" + guid + '/hotspots_all_genes'));
+						$(function() {
+							$.each(data.hotspots_top10, function(key, item){
+								$('<tr>').append(
+								$('<td>').text(key),
+								$('<td>').text((Math.round(item[0]*100)/100).toLocaleString()).css('font-family', 'Courier'),
+								$('<td>').text(item[1].toLocaleString()).css('font-family', 'Courier')).appendTo('#results_hotspots_tbody');
+							});
+						});
+
+						$('#see_100__hotspots').on('click', function() {
+                                        		$('#results_hotspots_tbody').empty();
+                                        		$.each(data.hotspots_top100, function(key, item){
+								$('<tr>').append(
+								$('<td>').text(key),
+								$('<td>').text(Math.round(item[0]*100)/100),
+								$('<td>').text(item[1].toLocaleString()).css('font-family', 'Courier')).appendTo('#results_hotspots_tbody');
+							});
+							$('#see_100__hotspots').hide();
+							$('#collapse__hotspots').show();
+						});
+
+						$('#collapse__hotspots').on('click', function(){
+							$('#results_hotspots_tbody').empty();
+							$.each(data.hotspots_top10, function(key, item){
+								$('<tr>').append(
+								$('<td>').text(key),
+								$('<td>').text(Math.round(item[0]*100)/100),
+								$('<td>').text(item[1].toLocaleString()).css('font-family', 'Courier')).appendTo('#results_hotspots_tbody');
+							});
+							$('#see_100__hotspots').show();
+							$('#collapse__hotspots').hide();
+						});
+
+						var hotspot_bar = document.getElementById('hotspot__barChart');
+						var bar_hotspot = new Chart(hotspot_bar, {
+							type: 'bar',
+							data: {
+								labels: Object.keys(data.hotspot_graph.one_snp),
+								datasets: [{
+									label: 'Count of altORFs with one SNP',
+									data: Object.values(data.hotspot_graph.one_snp),
+									backgroundColor: '#f3a6fc',
+									average_impact: Object.values(data.mean_impact_per_bin.one_snp),
+								}, {
+									label: 'Count of altORFs with 2 to 10 SNPs',
+									data: Object.values(data.hotspot_graph.one_ten),
+									backgroundColor: '#cf5fe3',
+									average_impact: Object.values(data.mean_impact_per_bin.one_ten),
+								}, {
+									label: 'Count of altORFs with at least 10 SNPs',
+									data: Object.values(data.hotspot_graph.over_ten),
+									backgroundColor: '#7b198c',
+									average_impact: Object.values(data.mean_impact_per_bin.over_ten),
+								}]
+							},
+							options: { legend: {display: true}, layout: { padding: {left:0, right:0, top:0, bottom:0} }, scales:{ xAxes:[{scaleLabel: {display: true, labelString: "Portion of gene's variants in altORF"}, stacked: true}], yAxes: [{scaleLabel: {display: true, labelString: 'Count of altORFs'}, stacked: true}]}, title: { display: true, text:'Mutational hotspots on alternative proteins', fontSize:16}, tooltips: { callbacks: { afterLabel: function(t, d){ return 'Average impact of SNPs: ' + Math.round(d.datasets[t.datasetIndex].average_impact[t.index]*100)/100 } }} },
+						});
+					} else {
+						$('.results__altHotSpot_allnulls').css('display', 'grid');
+					}
 				} else {
-					$('.results__protein_allnulls').css('display', 'grid');
-				}
-
-				if (data.hotspots_allnulls == 'False') {
-					$('.results__altHotSpot').css('display', 'grid');
-					$('#see_all__hotspots').attr('href', ("/openvar/" + guid + '/hotspots_all_genes'));
-					$(function() {
-						$.each(data.hotspots_top10, function(key, item){
-							$('<tr>').append(
-							$('<td>').text(key),
-							$('<td>').text((Math.round(item[0]*100)/100).toLocaleString()).css('font-family', 'Courier'),
-							$('<td>').text(item[1].toLocaleString()).css('font-family', 'Courier')).appendTo('#results_hotspots_tbody');
-						});
-					});
-
-					$('#see_100__hotspots').on('click', function() {
-                                        	$('#results_hotspots_tbody').empty();
-                                        	$.each(data.hotspots_top100, function(key, item){
-							$('<tr>').append(
-							$('<td>').text(key),
-							$('<td>').text(Math.round(item[0]*100)/100),
-							$('<td>').text(item[1].toLocaleString()).css('font-family', 'Courier')).appendTo('#results_hotspots_tbody');
-						});
-						$('#see_100__hotspots').hide();
-						$('#collapse__hotspots').show();
-					});
-
-					$('#collapse__hotspots').on('click', function(){
-						$('#results_hotspots_tbody').empty();
-						$.each(data.hotspots_top10, function(key, item){
-							$('<tr>').append(
-							$('<td>').text(key),
-							$('<td>').text(Math.round(item[0]*100)/100),
-							$('<td>').text(item[1].toLocaleString()).css('font-family', 'Courier')).appendTo('#results_hotspots_tbody');
-						});
-						$('#see_100__hotspots').show();
-						$('#collapse__hotspots').hide();
-					});
-
-					var hotspot_bar = document.getElementById('hotspot__barChart');
-					var bar_hotspot = new Chart(hotspot_bar, {
-						type: 'bar',
-						data: {
-							labels: Object.keys(data.hotspot_graph.one_snp),
-							datasets: [{
-								label: 'Count of altORFs with one SNP',
-								data: Object.values(data.hotspot_graph.one_snp),
-								backgroundColor: '#f3a6fc',
-								average_impact: Object.values(data.mean_impact_per_bin.one_snp),
-							}, {
-								label: 'Count of altORFs with 2 to 10 SNPs',
-								data: Object.values(data.hotspot_graph.one_ten),
-								backgroundColor: '#cf5fe3',
-								average_impact: Object.values(data.mean_impact_per_bin.one_ten),
-							}, {
-								label: 'Count of altORFs with at least 10 SNPs',
-								data: Object.values(data.hotspot_graph.over_ten),
-								backgroundColor: '#7b198c',
-								average_impact: Object.values(data.mean_impact_per_bin.over_ten),
-							}]
-						},
-						options: { legend: {display: true}, layout: { padding: {left:0, right:0, top:0, bottom:0} }, scales:{ xAxes:[{scaleLabel: {display: true, labelString: "Portion of gene's variants in altORF"}, stacked: true}], yAxes: [{scaleLabel: {display: true, labelString: 'Count of altORFs'}, stacked: true}]}, title: { display: true, text:'Mutational hotspots on alternative proteins', fontSize:16}, tooltips: { callbacks: { afterLabel: function(t, d){ return 'Average impact of SNPs: ' + Math.round(d.datasets[t.datasetIndex].average_impact[t.index]*100)/100 } }} },
-					});
-				} else {
-					$('.results__altHotSpot_allnulls').css('display', 'grid');
+					$('.results__altHotSpot').css('display', 'none');
+					$('.results__protein').css('display', 'none');
 				}
 
 			} else {
