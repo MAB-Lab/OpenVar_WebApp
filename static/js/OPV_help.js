d5abb17181ab9b9341317b32676c4d659ac5b922
MAB-Lab <marie.brunet@usherbrooke.ca>
2021-07-20 13:55:25 -0400

    test help

---
+++ static/js/OPV_help.js
@@ -0,0 +1,14 @@
+// Documentation page
+
+$(document).ready(function() {
+	$('.reveal_hide').click( function(event) {
+		event.preventDefault()
+		var qname = event.target.name;
+		if $('#' + qname).is(":hidden") {
+			$('#' + qname).attr('style', 'display: block;');
+		} else {
+			$('#' + qname).attr('style', 'display: none;');
+		}
+
+	});
+});
