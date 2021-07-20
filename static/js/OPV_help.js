cc4b36c49ab2a36ebc0552e0385e1f0dab5225ba
MAB-Lab <marie.brunet@usherbrooke.ca>
2021-07-20 14:05:14 -0400

    test help

--- static/js/OPV_help.js
+++ static/js/OPV_help.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	$('.reveal_hide').click( function(event) {
 		event.preventDefault()
 		var qname = event.target.name;
-		if ( $('#' + qname).is(":none") ) {
+		if ( $('#' + qname).css("display") == "none" ) {
 			$('#' + qname).attr('style', 'display: block;');
 		} else {
 			$('#' + qname).attr('style', 'display: none;');
